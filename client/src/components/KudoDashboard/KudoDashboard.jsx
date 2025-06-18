4ffe080bef90d99253a70820c5a0070eb3106c46
Isabella Marin <marin.isabella.10@gmail.com>
2025-06-18 12:32:56 -0700

    feat: made api call in frontend to get kudoboards

--- client/src/components/KudoDashboard/KudoDashboard.jsx
+++ client/src/components/KudoDashboard/KudoDashboard.jsx
@@ -2,56 +2,20 @@ import React from 'react';
 import KudoBoard from '../KudoBoard/KudoBoard';
 import './KudoDashboard.css';
 
-const KudoDashboard = () => {
-    const stockImage = 'https://picsum.photos/200/300';
-    // TODO: Replace with real data
-    const sampleBoards = [
-        {
-            id: 1,
-            image: stockImage,
-            kudosType: 'Thank You',
-            title: 'Thanks for your help!'
-        },
-        {
-            id: 2,
-            image: stockImage,
-            kudosType: 'Celebration',
-            title: 'Happy Birthday!'
-        },
-        {
-            id: 3,
-            image: stockImage,
-            kudosType: 'Inspiration',
-            title: 'Great job on the project!'
-        },
-        {
-            id: 4,
-            image: stockImage,
-            kudosType: 'Thank You',
-            title: 'Thanks for your support!'
-        },
-        {
-          id: 5,
-          image: stockImage,
-          kudosType: 'Celebration',
-          title: 'Congratulations on your achievement!'
-        }
-    ];
-
+const KudoDashboard = ({ kudoboards }) => {
     return (
         <div className="kudo-dashboard">
             <button className="create-new-kudo-board">
                 Create a New Kudo Board
             </button>
             <div className="kudo-boards-container">
-                {sampleBoards.map(board => (
-                    <KudoBoard
-                        key={board.id}
-                        image={board.image}
-                        kudosType={board.kudosType}
-                        title={board.title}
-                    />
-                ))}
+                { !kudoboards || kudoboards.length === 0 ? (
+                    <div className="no-boards-message">No boards to show</div>
+                ) : (
+                    kudoboards.map(kudoboard => (
+                        <KudoBoard key={kudoboard.id} image={kudoboard.gif} category={kudoboard.category} title={kudoboard.title} author={kudoboard.author}/>
+                    ))
+                )}
             </div>
         </div>
     );
